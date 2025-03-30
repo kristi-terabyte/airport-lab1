dcd8d36ae02207c9858412447ee4b4f07e495f12
kristi-terabyte <kristinabulavin2006@gmail.com>
2025-03-30 10:59:21 +0300

    Manufacturer added

---
+++ src/main/java/com/airport/Manufacturer.java
@@ -0,0 +1,11 @@
+package com.airport;
+
+public class Manufacturer {
+    private final String name;
+    private final String country;
+
+    public Manufacturer(final String name, final String country) {
+        this.name = name;
+        this.country = country;
+    }
+}
